d699e2d8bf526af5dd0db3e1c7e3809eae734be0
mnash29 <mtntrades10@gmail.com>
2019-12-10 16:13:53 -0800

    Added and configured middleware

--- StarChart/Startup.cs
+++ StarChart/Startup.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using StarChart.data;
+using StarChart.Data;
 using Microsoft.EntityFrameworkCore;
 
 namespace StarChart
